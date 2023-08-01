34241b2c479850e57fedf4781052fa4268f1efcd
Yap Jia Hong <yapjiahong2003@gmail.com>
2023-08-01 10:40:41 +0800

    feat(overhaul): Started overhaul

--- src/constants/constants.rs
+++
@@ -1,4 +0,0 @@
-use egui::Color32;
-
-pub const DEFAULT_TAG_COLOR: Color32 = Color32::TRANSPARENT;
-pub const EMPTY_TAG: &str = "  ";
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,49 @@
-// This is used to hide the console from popping up on Windows.
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-#![feature(drain_filter)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-pub mod app;
-use app::App;
+use eframe::egui;
 
-pub mod user;
-pub mod screens;
-pub mod constants;
-
-use eframe;
-
-// When compiling natively:
-#[cfg(not(target_arch = "wasm32"))]
-fn main() {
-    use egui::Vec2;
-
-    let native_options = eframe::NativeOptions {
-        min_window_size: Some(Vec2::new(498.0, 394.0)),
+fn main() -> Result<(), eframe::Error> {
+    // env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(320.0, 240.0)),
         ..Default::default()
     };
-
     eframe::run_native(
-        "Actt",
-        native_options,
-        Box::new(|cc| Box::new(App::new(cc))),
-    );
+        "My egui App",
+        options,
+        Box::new(|_cc| Box::<MyApp>::default()),
+    )
 }
+
+struct MyApp {
+    name: String,
+    age: u32,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            name: "Arthur".to_owned(),
+            age: 42,
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Click each year").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+        });
+    }
+}
+
