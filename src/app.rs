1794f661fc51aa9b9d200d6fd5b5e3d953cfc3e5
Yap Jia Hong <yapjiahong2003@gmail.com>
2023-08-13 13:15:19 +0800

    chore(org): implement screens for app
    Previously screens was separated from the app struct. Now, the functions
    repsonsible for switching screens are implemented under the app struct
    in a `screens.rs`

--- src/app.rs
+++ src/app.rs
@@ -1,75 +1,12 @@
 use crate::screens::*;
 
-use std::time::{Duration, Instant};
-
-use dirs::config_dir;
-use egui::Color32;
-
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
 pub struct App {
-    /// Even if it is unsued, ignore this warning. This is useful for passing in data.
-    /// This way I won't need to keep redeclaring it.
-    pub tmp: String,
-
     /// Name of the current activity.
     pub activity_name: String,
     /// The tag of the current activity.
     pub tag_name: String,
 
-    /// A tally of how long the user paused an activity throughout the
-    /// entire run of the activity.
-    pub total_pause_time: Duration,
-    /// The amount of time the user is pause for each time, instead of throughout
-    /// the entire run of the activity.
-    pub pause_time: Option<Instant>,
-    /// The amount of time passed when conducting the activity.
-    /// `total_time = pause_time + work_time`
-    pub total_time: Option<Instant>,
-    /// The amount of time the user is worked on an activity.
-    pub work_time: Duration,
-
-    // This group is for changing the name of an activity.
-    /// The dialog box to be shown when the user when they want
-    /// to change the name of an activity in the history screen.
-    pub show_name_assign_dialog: bool,
-    // The new name of the activity.
-    pub new_name: String,
-    /// The index of `new_name`. Check how `Config` stores data.
-    pub target_name_index: usize,
-
-    // This group of tags is used in the `activity_history` function.
-    // This is used when the user wishes to create a new tag and assign it
-    // to an activity that does not have a tag.
-    /// Check to see if a new tag needs to be created.
-    pub create_tag: bool,
-    /// Check to see if the user wants to switch to a new tag.
-    /// Determines whether the dialogue to create a tag will appear on screen.
-    pub show_create_tag_win: bool,
-
-    pub change_tag: bool,
-    /// Determines whether the dialogue to change a tag will appear on screen.
-    pub show_change_tag_win: bool,
-
-    /// The new tag the user creates.
-    pub new_tag: String,
-    /// The original tag to be changed
-    pub target_tag: String,
-    /// The index of `target_tag`
-    pub target_tag_index: usize,
-    /// UNUSED.
-    /// Boolean to determine whether to show the color picker on screen.
-    pub show_color_picker: bool,
-
-    // Misc. Ungrouped fields that don't belong to a particular group.
-    /// Keeps track of which screen the user is currently on.
     pub screen: Screen,
-    /// The message to display when there is an error of some sort.
-    pub warning: Option<String>,
-
-    /// Color for tags
-    pub color: Color32,
-    /// Identifies which text box should be focused.
-    pub focus: bool,
 }
 
 impl App {
@@ -84,48 +21,18 @@ impl eframe::App for App {
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         match self.screen {
-            Screen::Start => start_screen(self, ctx, _frame),
-            Screen::Tracking => start_screen(self, ctx, _frame),
+            Screen::Start => self.start_screen(ctx, _frame),
+            Screen::Tracking => self.start_screen(ctx, _frame),
         }
     }
 }
 
 impl Default for App {
     fn default() -> Self {
-        // Gets the home directory (platform independent)
-        let home = config_dir().unwrap();
-        let home = format!("{}/actt", home.display());
-
         Self {
-            tmp: "".to_string(),
             activity_name: "".to_string(),
             tag_name: "".to_string(),
-
-            total_pause_time: Duration::from_secs(0),
-            pause_time: None,
-            total_time: None,
-            work_time: Duration::from_secs(0),
-
-            show_name_assign_dialog: false,
-            new_name: "".to_string(),
-            target_name_index: usize::MAX,
-
-            create_tag: false,
-            show_create_tag_win: false,
-
-            change_tag: false,
-            show_change_tag_win: false,
-
-            target_tag: "".to_string(),
-            target_tag_index: 0,
-            new_tag: "".to_string(),
-            show_color_picker: false,
-
             screen: Screen::Start,
-            warning: None,
-
-            color: Color32::BLACK,
-            focus: false,
         }
     }
 }
--- src/screens.rs
+++ src/screens.rs
@@ -1,11 +1,6 @@
 use crate::app::App;
 
-use crate::constants::*;
-
-use egui::{
-    color_picker::{color_picker_color32, Alpha},
-    Ui, Vec2,
-};
+use egui::{Ui, Vec2};
 
 #[derive(PartialEq)]
 pub enum Screen {
@@ -13,68 +8,59 @@ pub enum Screen {
     Tracking,
 }
 
-pub fn horizontal_menu(app: &mut App, ui: &mut Ui) {
-    ui.horizontal_top(|ui| {
-        ui.selectable_value(&mut app.screen, Screen::Start, "Home");
-    });
-
-    ui.separator();
-}
-
-/// The start screen is where metadata about an activity is set.
-pub fn start_screen(app: &mut App, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-    // There's nothing wrong with the return type. It's just that `CentralPanel` is also a function
-    // Which means that the return type needs to cover that as well.
-    egui::CentralPanel::default().show(ctx, |ui| {
-        horizontal_menu(app, ui);
-        ui.with_layout(egui::Layout::top_down(egui::Align::Center), |ui| {
-            ui.heading("Home");
-            match &app.warning {
-                None => ui.label("\n\n\n"),
-                Some(msg) => {
-                    let msg = format!("\n{}\n\n", msg);
-                    ui.label(msg)
-                }
-            };
-
-            ui.allocate_ui_with_layout(
-                Vec2::new(200.0, 200.0),
-                egui::Layout::top_down(egui::Align::Center),
-                |ui| {
-                    // 1 column per action.
-                    ui.columns(2, |column| {
-                        column[0].vertical_centered_justified(|ui| ui.label("Activity"));
-                        column[1].vertical_centered_justified(|ui| {
-                            ui.text_edit_singleline(&mut app.activity_name)
-                                .on_hover_text("What do you want to track?")
-                        });
-                    });
-
-                    ui.columns(2, |column| {
-                        column[0].vertical_centered_justified(|ui| ui.label("Tag"));
-                        column[1].vertical_centered_justified(|ui| {
-                            // This is needed for when a tag is delete. Since an empty tag is two
-                            // blank spaces, in the tag text edit there will be two spaces
-                            // inserted. This is a fix for it.
-                            if app.tag_name == EMPTY_TAG {
-                                app.tag_name = "".to_string();
-                            }
-                        });
-                    });
-
-                    ui.columns(2, |column| {
-                        column[0].vertical_centered_justified(|ui| ui.label("Tag color"));
-                        column[1].vertical_centered_justified(|ui| {
-                            color_picker_color32(ui, &mut app.color, Alpha::Opaque);
-                        });
-                    });
-                },
-            );
-
-            ui.label("\n");
-            if ui.button("Start").clicked() {
-                unimplemented!();
-            }
+impl App {
+    pub fn horizontal_menu(&mut self, ui: &mut Ui) {
+        ui.horizontal_top(|ui| {
+            ui.selectable_value(&mut self.screen, Screen::Start, "Home");
         });
-    });
+
+        ui.separator();
+    }
+
+    /// The start screen is where metadata about an activity is set.
+    pub fn start_screen(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // There's nothing wrong with the return type. It's just that `CentralPanel` is also a function
+        // Which means that the return type needs to cover that as well.
+        egui::CentralPanel::default().show(ctx, |ui| {
+            self.horizontal_menu(ui);
+            ui.with_layout(egui::Layout::top_down(egui::Align::Center), |ui| {
+                ui.heading("Home");
+                ui.label("\n\n\n");
+
+                ui.allocate_ui_with_layout(
+                    Vec2::new(200.0, 200.0),
+                    egui::Layout::top_down(egui::Align::Center),
+                    |ui| {
+                        // Activity column
+                        ui.columns(2, |column| {
+                            column[0].vertical_centered_justified(|ui| ui.label("Activity"));
+                            column[1].vertical_centered_justified(|ui| {
+                                ui.text_edit_singleline(&mut self.activity_name)
+                                    .on_hover_text("What do you want to track?")
+                            });
+                        });
+
+                        // Tag column
+                        ui.columns(2, |column| {
+                            column[0].vertical_centered_justified(|ui| ui.label("Tag"));
+                            column[1].vertical_centered_justified(|ui| {});
+                        });
+
+                        // Color column
+                        ui.columns(2, |column| {
+                            column[0].vertical_centered_justified(|ui| ui.label("Tag color"));
+                            column[1].vertical_centered_justified(|ui| {
+                                ui.label("Insert color picker here.");
+                            });
+                        });
+                    },
+                );
+
+                ui.label("\n");
+                if ui.button("Start").clicked() {
+                    self.screen = Screen::Tracking;
+                }
+            });
+        });
+    }
 }
